3226760240bef6a1ccd5a6fc3847e2381b91c902
Slayer <aksuplayz1337@gmail.com>
2021-04-03 16:30:52 +0300

    Report command (should be stable)

---
+++ src/commands/core/report.js
@@ -0,0 +1,33 @@
+const Command = require('../../structures/Command');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            description: 'Checks the user\'s information in DDUB'
+        });
+    }
+
+    async run(message, args) {
+        if (!args.length) return message.channel.createMessage('Please give a user\'s id to report!')
+        const match = message.content.match(/\d{18}/);
+        let member = match ? await this.bot.getRESTUser(match[0]) : null
+
+        if (!member) return message.channel.createMessage('That user was not found!')
+
+        const report = await fetch(`https://discord.riverside.rocks/report.json.php?id=${member.id}&key=${process.env.DDUB_TOKEN}&details=${args[1]} (Reported by ${message.author.tag})`).then(res => res.json())
+
+        if (!report) return message.channel.createMessage('Report failed. DDUB didn\'t receive the request!')
+        if (report.message === 'You can only report a user every 10 minutes.') return message.channel.createMessage('This user has been already reported within the 10 minutes!')
+
+        message.channel.createMessage({
+            embed: {
+                color: 0x7289DA,
+                url: `https://discord.riverside.rocks/check?id=${member.id}`,
+                title: 'User Reported!',
+                description: `${member.username} was reported successfully!`,
+
+            }
+        })
+    }
+};
